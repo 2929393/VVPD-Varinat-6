e699a8c14588ccc75a26051134c2de21e571b345
2929393 <nikita.kozhevnikov2707@mail.ru>
2022-12-20 00:56:03 +0700

    Добавил класс числа с его системой счисления

---
+++ src/Number.java
@@ -0,0 +1,9 @@
+public class Number {
+    private String value;
+    private int numberSystem;
+
+    public Number(String value, int numberSystem) {
+        this.value = value;
+        this.numberSystem = numberSystem;
+    }
+}

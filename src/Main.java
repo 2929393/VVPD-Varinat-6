c91f00fce73bbea9b7f4f4e1099f2818bd4832c9
2929393 <nikita.kozhevnikov2707@mail.ru>
2022-12-20 01:08:05 +0700

    Метод для получения системы счисления

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int menuKey;
+        do {
+            System.out.println("Выберите систему вводимого счисления: 2, 8, 10, 16");
+            int numberSystem = getNumberSystem(input);
+            menuKey = getValue(input);
+        }
+        while (menuKey != 0);
     }
 
-    public static String getValue(Scanner input) {
+    public static int getValue(Scanner input) {
         String result;
         while (!isCorrectValue(result = input.nextLine())) {
             System.out.println("Введите число");
         }
-        return result;
+        return Integer.parseInt(result);
     }
 
     public static boolean isCorrectValue(String line) {
@@ -21,5 +28,22 @@ public class Main {
         catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    public static int getNumberSystem(Scanner input) {
+        int result;
+        while (!isNumberSystem(result = getValue(input))) {
+            System.out.println("Введите систему счисления");
+        }
+        return result;
+    }
+
+    public static boolean isNumberSystem(int value) {
+        int[] numberSystems = new int[] {2, 8, 10, 16};
+        for (int numberSystem : numberSystems) {
+            if (value == numberSystem)
+                return true;
+        }
+        return false;
     }
 }

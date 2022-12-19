923a4a97c1172fb8591813868dc90b3c26d9004d
2929393 <nikita.kozhevnikov2707@mail.ru>
2022-12-20 01:00:09 +0700

    Методы для получения пользовательского ввода

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        /**
-         * Create
-         */
+
+    }
+
+    public static String getValue(Scanner input) {
+        String result;
+        while (!isCorrectValue(result = input.nextLine())) {
+            System.out.println("Введите число");
+        }
+        return result;
+    }
+
+    public static boolean isCorrectValue(String line) {
+        try {
+            Integer.parseInt(line);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
     }
 }

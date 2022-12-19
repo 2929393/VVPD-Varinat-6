1ef36002bcdac773dad8e42a00384edd25823cb3
2929393 <nikita.kozhevnikov2707@mail.ru>
2022-12-20 02:00:57 +0700

    Написал комментарии

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * Объект класса Number, представляющего собой число в некоторой системе счисления
+     */
     static Number value;
     public static void main(String[] args) {
+        //Создание сканера для ввода в консоль
         Scanner input = new Scanner(System.in);
+        //Ввод первоначального числа
         createNumber(input);
+
+        //Вывод меню. Цикл будет повторяться, пока пользователь не выберет "Выход"
         int menuKey;
         do {
             System.out.println(value);
@@ -33,6 +40,10 @@ public class Main {
         while (menuKey != 0);
     }
 
+    /**
+     * Метод для ввода числа
+     * @param input Сканер ввода
+     */
     public static void createNumber(Scanner input) {
         System.out.println("Выберите систему вводимого счисления: 2, 8, 10, 16");
         int numberSystem = getNumberSystem(input);
@@ -40,53 +51,95 @@ public class Main {
         value = new Number(getValue(input, numberSystem), numberSystem);
     }
 
+    /**
+     * Метод для конвертации значения из одной системы счисления в другую
+     * @param from Система счисления, из которой надо перевести
+     * @param to Система счисления, в которую надо перевести
+     */
     public static void convert(int from, int to) {
+        //Проверка, что система счисления числа является подходящей для данной конвертации
         if (value.getNumberSystem() == from) {
             String result = value.getValue();
+            //Перевод числа в десятичную систему счисления. Это обязательный шаг
             int decimal = Integer.parseInt(value.getValue(), from);
             switch (to) {
+                //Перевод в 2-ную систему
                 case 2 -> result = Integer.toBinaryString(decimal);
+                //Перевод в 8-ную систему
                 case 8 -> result = Integer.toOctalString(decimal);
+                //Перевод в 16-ную систему
                 case 16 -> result = Integer.toHexString(decimal);
+                //Перевод в 10-ную отсутствует. Просто возвращается число ранее переведенное в десятичную
                 case 10 -> result = String.valueOf(decimal);
             }
+            //Изменение значений объекта числа
             value.setValue(result);
             value.setNumberSystem(to);
         } else System.out.println("Система счисления должны быть " + from + "-ной");
     }
 
+    /**
+     * Метод для получения числа в определенной системе счисления
+     * @param input Сканер ввода
+     * @param numberSystem Система счисления
+     * @return Строка, содержащая число в системе счисления
+     */
     public static String getValue(Scanner input, int numberSystem) {
         String result;
+        //Метод isCorrectValue проверяет, что это число, и оно в нужной системе счисления
         while (!isCorrectValue(result = input.nextLine(), numberSystem)) {
             System.out.println("Введите число");
         }
         return result;
     }
 
+    /**
+     * Метод для проверки корректности числа
+     * @param line Строка, содержащая число
+     * @param numberSystem Система счисления
+     * @return Результат проверки
+     */
     public static boolean isCorrectValue(String line, int numberSystem) {
         try {
+            //Если это не число или число в другой системе счисления, сгенерируется исключение
             Integer.parseInt(line, numberSystem);
+            //Если число прошло проверку, возвращается Истина
             return true;
         }
         catch (NumberFormatException e) {
+            //В случае исключения строка не прошла проверку. Возвращается Ложь
             return false;
         }
     }
 
+    /**
+     * Метод для получения системы счисления
+     * @param input Сканер ввода
+     * @return Система счисления
+     */
     public static int getNumberSystem(Scanner input) {
         int result;
+        //Пользователь вводит число системы счисления. Метод isNumberSystem проверяет, что это 2, 9, 10 или 16.
         while (!isNumberSystem(result = Integer.parseInt(getValue(input, 10)))) {
+            //Если это ни одно из вышеперечисленных значений, выдается ошибка
             System.out.println("Введите систему счисления");
         }
         return result;
     }
 
+    /**
+     * Метод для проверки введенной системы счисления, как обрабатываемой
+     * @param value Номер системы счисления
+     * @return Результат проверки
+     */
     public static boolean isNumberSystem(int value) {
         int[] numberSystems = new int[] {2, 8, 10, 16};
         for (int numberSystem : numberSystems) {
+            //Если полученная система - это 2, 8, 10, 16, возвращается Истина
             if (value == numberSystem)
                 return true;
         }
+        //Если нет, возвращается Ложь
         return false;
     }
 }
